5a7b55c72225951eaab564ea57275623ca80901a
HarmanGill <harman97gill@gmail.com>
2024-05-13 14:50:27 -0400

    server running hello world

---
+++ controllers/user.js
@@ -0,0 +1,12 @@
+// const express = require('express')
+// const router = express.Router()
+// // const User = require('../models/user.js')
+
+
+// // INDEX
+// // router.get('/', async (req, res) => {
+// //   const foundUsers = await User.find()
+// //   res.render('user.json')
+// // })
+
+// module.exports = router
--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 // Import Dependencies
-const express = require("express");
-const cors = require("cors");
+// get .env variables 
+require("dotenv").config()
+// get PORT from .env , give a default value of 3000 
+// if the PORT does not exist 
+const { DATABASE_URL, PORT = 3000} = process.env
+// import express 
+const express = require('express')
+// create application object 
+const app = express()
+// import mongoose 
+const mongoose = require('mongoose')
+// import middleware 
+const cors = require('cors')
+const morgan = require('morgan')
 
 // Import JSON files
 // const projects = require("./projects.json");
 // const about = require("./about.json");
 
-// Create our app object
-const app = express();
-
 // set up middleware
-app.use(cors());
+// app.use(cors());
+
+
+// CONTROLLERS
+// const userController = require('./controllers/user.js')
+// app.use('/user', userController)
+// const accountController = require('./controllers/account.js')
+// app.use('/account', accountController)
 
 //home route for testing our app
 app.get("/home", (req, res) => {
@@ -20,17 +36,23 @@ app.get("/home", (req, res) => {
 // route for retrieving projects
 app.get("/user", (req, res) => {
   // send projects via JSON
-  res.json(projects);
+  res.send('hi')
 });
 
 // route for retrieving about info
-app.get("/about", (req, res) => {
-  // send projects via JSON
-  res.json(about);
-});
+// app.get("/about", (req, res) => {
+//   // send projects via JSON
+//   res.json(about);
+// });
 
-//declare a variable for our port number
-const PORT = process.env.PORT || 4000;
+
+
+// CONNECT TO MONGO
+// mongoose.connect(mongoURI)
+// const db = mongoose.connection
+// db.on('error', (err) => console.log(err.message + ' error with mongo connection'))
+// db.on('connected', () => console.log('mongo is connected'))
+// db.on('disconnected', () => console.log('mongo disconnected'))
 
 // turn on the server listener
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
